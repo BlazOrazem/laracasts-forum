1a91d7fb350b89bd01263780517c523d130d894b
Blaž Oražem <blaz@orazem.info>
2017-12-29 20:08:24 +0100

    Ep 4 - A User May Respond to Threads

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/{thread}', 'ThreadsController@show');
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
--- tests/Feature/ReadsThreadsTest.php
+++ tests/Feature/ReadsThreadsTest.php
@@ -28,7 +28,7 @@ class ReadsThreadsTest extends TestCase
     /** @test */
     function a_user_can_read_a_single_thread()
     {
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
             ->assertSee($this->thread->title);
     }
 
@@ -38,7 +38,7 @@ class ReadsThreadsTest extends TestCase
         $reply = factory('App\Reply')
             ->create(['thread_id' => $this->thread->id]);
 
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
             ->assertSee($reply->body);
     }
 }
