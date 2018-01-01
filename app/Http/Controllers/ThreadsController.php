f27c0ce8dbf5ae2f4eeffe86c2399046c8cedc66
Blaž Oražem <blaz@orazem.info>
2018-01-01 16:04:46 +0100

    Ep 8 - The Exception Handling Conundrum

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -12,7 +12,7 @@ class ThreadsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->only('store');
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     /**
@@ -34,7 +34,7 @@ class ThreadsController extends Controller
      */
     public function create()
     {
-        //
+        return view('threads.create');
     }
 
     /**
