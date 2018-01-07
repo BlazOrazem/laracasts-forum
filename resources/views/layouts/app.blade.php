dc2a352e3962bc199d256444d5761823e99812d5
Blaž Oražem <blaz@orazem.info>
2018-01-07 14:06:13 +0100

    Ep 13 - Extracting to View Composers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
                             </a>
 
                             <ul class="dropdown-menu">
-                                @foreach(App\Channel::all() as $channel)
+                                @foreach($channels as $channel)
                                     <li><a href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a></li>
                                 @endforeach
                             </ul>
