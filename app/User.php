0440ff4cbe5e3ccac81ef223fc7e8f3d6d431892
Blaž Oražem <blaz@orazem.info>
2018-01-17 19:00:09 +0100

    Ep 22 - A User Has a Profile

--- app/User.php
+++ app/User.php
@@ -26,4 +26,24 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the route key name for Laravel.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
+
+    /**
+     * Fetch all threads that were created by the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function threads()
+    {
+        return $this->hasMany(Thread::class)->latest();
+    }
 }
