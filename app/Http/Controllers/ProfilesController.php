e57e5ffdad0f5acfb50d4e30cb5d403760a426af
Blaž Oražem <blaz@orazem.info>
2018-01-19 18:58:25 +0100

    Ep 26 - How to Construct An Activity Feed with TDD: Part 2

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,11 +6,32 @@ use App\User;
 
 class ProfilesController extends Controller
 {
+    /**
+     * Show the user's profile.
+     *
+     * @param User $user
+     *
+     * @return \Illuminate\View\View
+     */
     public function show(User $user)
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'threads'     => $user->threads()->paginate(30),
+            'activities'  => $this->getActivity($user),
         ]);
+    }
+
+    /**
+     * Get user's activity.
+     *
+     * @param User $user
+     *
+     * @return mixed
+     */
+    protected function getActivity(User $user)
+    {
+        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
     }
 }
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,34 +2,23 @@
 
 @section('content')
     <div class="container">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="page-header">
-                <h1>
-                    {{ $profileUser->name }}
-                    <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
-                </h1>
-            </div>
-
-            @foreach ($threads as $thread)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="level">
-                            <span class="flex">
-                                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                                <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                            </span>
-
-                            <span>{{ $thread->created_at->diffForHumans() }}</span>
-                        </div>
-                    </div>
-
-                    <div class="panel-body">
-                        {{ $thread->body }}
-                    </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="page-header">
+                    <h1>
+                        {{ $profileUser->name }}
+                        <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
+                    </h1>
                 </div>
-            @endforeach
 
-            {{ $threads->links() }}
+                @foreach ($activities as $date => $activity)
+                    <h3 class="page-header">{{ $date }}</h3>
+
+                    @foreach ($activity as $record)
+                        @include ("profiles.activities.{$record->type}", ['activity' => $record])
+                    @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
