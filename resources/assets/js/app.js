31c6e0d02d073aa2f452b245627192aff8ef34b9
Blaž Oražem <blaz@orazem.info>
2018-01-21 22:16:19 +0100

    Ep 36 - A More Data-centric Approach

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,13 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+Vue.prototype.authorize = function (handler) {
+    // Additional admin privileges
+    let user = window.App.user;
+
+    return user ? handler(user) : false;
+};
+
 window.events = new Vue();
 
 window.flash = function (message) {
@@ -22,7 +29,8 @@ window.flash = function (message) {
  */
 
 Vue.component('flash', require('./components/Flash.vue'));
-Vue.component('reply', require('./components/Reply.vue'));
+
+Vue.component('thread-view', require('./pages/Thread.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/pages/Thread.vue
@@ -0,0 +1,34 @@
+<script>
+    import Replies from '../components/Replies.vue';
+
+    export default {
+        props: ['initialRepliesCount'],
+
+        components: { Replies },
+
+        data() {
+            return {
+                repliesCount: this.initialRepliesCount
+            };
+        }
+        //
+        // methods: {
+        //     update() {
+        //         axios.patch('/replies/' + this.data.id, {
+        //             body: this.body
+        //         });
+        //
+        //         this.editing = false;
+        //
+        //         flash('Updated!');
+        //     },
+        //     destroy() {
+        //         axios.delete('/replies/' + this.data.id);
+        //
+        //         $(this.$el).fadeOut(300, () => {
+        //             flash('Your reply has been deleted.');
+        //         });
+        //     }
+        // }
+    }
+</script>
