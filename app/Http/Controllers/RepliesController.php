6e66e75356cfe6804b4c446d7504e5fe4f6e5837
Blaž Oražem <blaz@orazem.info>
2018-02-08 13:29:36 +0100

    Ep 51 - Spam Detection At All Ports

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -19,8 +19,8 @@ class RepliesController extends Controller
     /**
      * Fetch all relevant replies.
      *
-     * @param int    $channelId
-     * @param Thread $thread
+     * @param integer $channelId
+     * @param Thread  $thread
      *
      * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
      */
@@ -34,16 +34,12 @@ class RepliesController extends Controller
      *
      * @param integer $channelId
      * @param Thread  $thread
-     * @param Spam    $spam
      *
-     * @return \Illuminate\Http\RedirectResponse
-     *
-     * @throws \Exception
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Http\RedirectResponse
      */
-    public function store($channelId, Thread $thread, Spam $spam)
+    public function store($channelId, Thread $thread)
     {
-        $this->validate(request(), ['body' => 'required']);
-        $spam->detect(request('body'));
+        $this->validateReply();
 
         $reply = $thread->addReply([
             'body'    => request('body'),
@@ -68,7 +64,7 @@ class RepliesController extends Controller
     {
         $this->authorize('update', $reply);
 
-        $this->validate(request(), ['body' => 'required']);
+        $this->validateReply();
 
         $reply->update(request(['body']));
     }
@@ -94,5 +90,15 @@ class RepliesController extends Controller
         }
 
         return back();
+    }
+
+    /**
+     * Validate the incoming reply.
+     */
+    protected function validateReply()
+    {
+        $this->validate(request(), ['body' => 'required']);
+
+        resolve(Spam::class)->detect(request('body'));
     }
 }
